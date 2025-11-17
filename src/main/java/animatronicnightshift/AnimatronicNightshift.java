55478f313f50f35acb1dc95f1c57001691ac65ff
SpriSan <spriyoutube@outlook.fr>
2025-11-17 19:23:33 +0100

    first commit

---
+++ src/main/java/animatronicnightshift/AnimatronicNightshift.java
@@ -0,0 +1,40 @@
+package animatronicnightshift;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(AnimatronicNightshift.MODID)
+public class AnimatronicNightshift
+{
+    public static final String MODID = "cannabyssal";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public AnimatronicNightshift()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
